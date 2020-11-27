526d935ac6bcf87ad16e37c4e3186dc5cb725a36
LonelyDragon <zavodchikov_aa@mail.ru>
2020-11-27 10:40:06 +0300

    sync

--- articles/urls.py
+++ articles/urls.py
@@ -1,9 +1,19 @@
 from django.urls import path
 
 
-from .views import ArticleListView
+from .views import (
+    ArticleListView,
+    ArticleDetailView,
+    ArticleEditView,
+    ArticleDeleteView,
+    ArticleCreateView,
+)
 
 
 urlpatterns = [
+    path('<int:pk>/detail/', ArticleDetailView.as_view(), name='article_detail'),
+    path('<int:pk>/edit/', ArticleEditView.as_view(), name='article_edit'),
+    path('<int:pk>/delete/', ArticleDeleteView.as_view(), name='article_delete'),
+    path('new/', ArticleCreateView.as_view(), name='article_new'),
     path('', ArticleListView.as_view(), name='article_list')
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,4 +1,6 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import UpdateView, DeleteView, CreateView
+from django.urls import reverse_lazy
 
 
 from .models import Article
@@ -7,3 +9,26 @@ from .models import Article
 class ArticleListView(ListView):
     model = Article
     template_name = 'article_list.html'
+
+class ArticleDetailView(DetailView):
+    model = Article
+    template_name = 'article_detail.html'
+
+class ArticleEditView(UpdateView):
+    model = Article
+    fields = ('title', 'body',)
+    template_name = 'article_edit.html'
+
+class ArticleDeleteView(DeleteView):
+    model = Article
+    template_name = 'article_delete.html'
+    success_url = reverse_lazy('article_list')
+
+class ArticleCreateView(CreateView):
+    model = Article
+    fields = ('title', 'body')
+    template_name = 'article_new.html'
+    
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
