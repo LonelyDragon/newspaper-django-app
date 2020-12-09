73f5fa9c398f27b01fa447286cb0c84af8adf240
LonelyDragon <zavodchikov_aa@mail.ru>
2020-12-09 13:00:31 +0300

    edit views.py - ading comment_edit view

--- articles/views.py
+++ articles/views.py
@@ -2,20 +2,18 @@ from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import UpdateView, DeleteView, CreateView
 from django.urls import reverse_lazy
+from django.http import Http404
 
 
-from .models import Article
+from .models import Article, Comment
 
 
 class ArticleListView(LoginRequiredMixin, ListView):
     model = Article
     template_name = 'article_list.html'
     login_url = 'login'
-    queryset = Article.objects.order_by('date')
+    queryset = Article.objects.order_by('-date')
     
-    def get_queryset(self):
-        return self.queryset.reverse()
-
 class ArticleDetailView(LoginRequiredMixin, DetailView):
     model = Article
     template_name = 'article_detail.html'
@@ -31,6 +29,22 @@ class ArticleEditView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
         obj = self.get_object()
         return obj.author == self.request.user
 
+class ArtilcleCommentAdd(LoginRequiredMixin, CreateView):
+    model = Comment
+    fields = ('comment', )
+    template_name = 'article_comment_add.html'
+    login_url = 'login'
+
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        try:
+            form.instance.article = Article.objects.get(pk=self.kwargs.get("pk"))
+        except Article.DoesNotExist:
+            raise Http404("Article does not exist")
+        return super().form_valid(form)
+    
+    
 class ArticleDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Article
     template_name = 'article_delete.html'
