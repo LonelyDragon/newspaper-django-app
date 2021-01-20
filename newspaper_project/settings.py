0361e92b5f99c759784c9460a85ab17ce7a4637b
LonelyDragon <zavodchikov_aa@mail.ru>
2021-01-20 19:23:00 +0300

    add lost variable

--- newspaper_project/settings.py
+++ newspaper_project/settings.py
@@ -140,7 +140,7 @@ LOGOUT_REDIRECT_URL = 'home'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-
+EMAIL_HOST = os.getenv('EMAIL_HOST')
 DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
